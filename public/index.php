206bee594384c93a8e41eb0c01f26e45b0769fd4
FullCircle7 <thetremor@hotmail.co.uk>
2016-10-12 21:11:09 +0100

    - Changes made 12/10/2016 - .htaccess redirection testing

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+echo 'Hello! You are in the /var/www/shop/public directory.' . '<br><br>';
+
+echo 'The original request URL was... ' . '<b>' . $_SERVER['REQUEST_URI'] . '</b>';
+
+
+?>
+
+
+
